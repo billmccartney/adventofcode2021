c358b869740a3ba91fb22a48e07940d74bc485d4
billmccartney <billmccartney@gmail.com>
2021-12-13 21:33:49 -0500

    wrote part 2 processing for day 2 (simply modification of logic)

--- day2.py
+++ day2.py
@@ -14,10 +14,33 @@ def day2_part1_process(filename):
             elif(direction == "down"):
                 depth += count
 
-            line=f.readline()
+            line = f.readline()
+    return horizontal * depth
+
+
+def day2_part2_process(filename):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    with open(filename) as f:
+        line = f.readline()
+        while(line):
+            direction, count = line.strip().split(" ")
+            count = int(count)
+            if(direction == "forward"):
+                horizontal += count
+                depth += aim * count
+            elif(direction == "up"):
+                aim -= count
+            elif(direction == "down"):
+                aim += count
+
+            line = f.readline()
     return horizontal * depth
 
 
 if __name__ == "__main__":
     assert(day2_part1_process("day2/example.dat") == 150)
     print("part1 %d" % day2_part1_process("day2/raw.dat"))
+    assert (day2_part2_process("day2/example.dat") == 900)
+    print("part2 %d" % day2_part2_process("day2/raw.dat"))
